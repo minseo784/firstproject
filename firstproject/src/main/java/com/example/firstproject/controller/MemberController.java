215bf569b13b9a85db7acfae39e334088c58af1e
minseo <mseo5262004@gmail.com>
2025-02-20 14:27:02 +0900

    member 페이지 수정 기능 추가

--- firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -63,4 +63,31 @@ public class MemberController {
         // 3. 뷰 페이지 반환하기
         return "members/index";
     }
+
+    @GetMapping("/members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        // 1. 수정할 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("member", memberEntity);
+        // 3. 뷰 페이지 반환하기
+        return "members/edit";
+    }
+
+    @PostMapping("/members/update")
+    public String update(MemberForm form) {
+        log.info(form.toString());
+        // 1. DTO를 엔티티로 변환하기
+        Member memberEntity = form.toEntity();
+        log.info(memberEntity.toString());
+        // 2. 엔티티를 DB에 저장하기
+        // 2-1. DB에서 기존 데이터 가져오기
+        Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
+        // 2-2. 기존 데이터 값 갱신하기
+        if(target != null) {
+            memberRepository.save(memberEntity);
+        }
+        // 3. 수정 결과 페이지로 리다이렉트하기
+        return "redirect:/members/"+memberEntity.getId();
+    }
 }
